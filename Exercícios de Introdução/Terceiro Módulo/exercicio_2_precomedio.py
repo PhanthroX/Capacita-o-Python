d8fdc67c4479c624349ceb4cfa09ffe49e6ed91f
Matheus Mariano Rocha <105793853+PhanthroX@users.noreply.github.com>
2024-03-20 13:37:16 -0300

    Update exercicio_2_precomedio.py
    Ajuste de código

--- Exercícios de Introdução/Terceiro Módulo/exercicio_2_precomedio.py
+++ Exercícios de Introdução/Terceiro Módulo/exercicio_2_precomedio.py
@@ -7,5 +7,5 @@ df.info()
 
 dataframe_novo = pd.DataFrame(df, columns=["id", "name", "neighbourhood", "room_type", "price"])
 
-media = dataframe_novo.loc[dataframe_novo['room_type'] == 'Entire home/apt', 'price'].mean()
+media = dataframe_novo.loc[(dataframe_novo['room_type'] == 'Entire home/apt') & (dataframe_novo['neighbourhood'] == 'Barra da Tijuca'), 'price'].mean()
 print("preço médio = " + str(media))
