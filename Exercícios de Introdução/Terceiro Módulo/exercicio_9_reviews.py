63c0206e4a20bdbdd1a375eca5b1b2af0baa2b55
Matheus Mariano Rocha <105793853+PhanthroX@users.noreply.github.com>
2024-03-20 13:54:46 -0300

    Update exercicio_9_reviews.py

--- Exercícios de Introdução/Terceiro Módulo/exercicio_9_reviews.py
+++ Exercícios de Introdução/Terceiro Módulo/exercicio_9_reviews.py
@@ -7,4 +7,4 @@ df.info()
 
 dataframe_novo = pd.DataFrame(df, columns=["id", "neighbourhood", "number_of_reviews", "last_review"])
 
-display(dfn.sort_values("number_of_reviews", ascending=False).drop_duplicates("neighbourhood").head(10).filter(["id","last_review"]))
+display(df.sort_values("number_of_reviews", ascending=False).drop_duplicates("neighbourhood").head(10).filter(["id","last_review"]))
